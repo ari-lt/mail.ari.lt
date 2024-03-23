563acf6e210278cbe764be881624586fd5004026
Ari Archer <ari@ari.lt>
2024-03-23 22:42:35 +0200

    fix gunicorn
    Signed-off-by: Ari Archer <ari@ari.lt>

--- signup/run.sh
+++ signup/run.sh
@@ -3,7 +3,7 @@
 set -eu
 
 main() {
-    python3 -m pip install --upgrade --break-system-packages --user gunicorn
+    python3 -m pip install --upgrade gunicorn
     python3 -m gunicorn -b 127.0.0.1:13912 main:app &
     disown
 }
